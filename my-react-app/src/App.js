7e2c2bc6e6a5cb03ade442ca25809eb034756b28
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-06-09 23:32:23 +0200

    CreatingVariable&SeeingWebServrDetectingChanging

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const greeting = "Hello World";
   return (
     <div >
       <h1>Testing if it is Rendering out and printing the code!Hello World!</h1>
