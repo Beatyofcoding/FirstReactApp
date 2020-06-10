8183658c504420a6f5604e72a0f7559d427f1491
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-06-10 23:04:52 +0200

    TestingSpaceBetween&PrintingCorrectelyHelloWorld

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
   }
   return (
     <div >
-      <h1>{testing.testing_one}{testing.testing_two}</h1>
+      <h1>{testing.testing_one} {testing.testing_two}!</h1>
       {/* <h1>Testing if it is Rendering out and printing the code!Hello World!</h1> */}
     </div>
   );
