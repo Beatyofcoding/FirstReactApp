5ea2e875e8468c08a935081c47d551be35e880d3
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-06-11 22:51:04 +0200

    AddingArrayId3

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -19,8 +19,16 @@ const courses = [
     rating: 4.6,
     url: "https://codingthesmartway.com/courses/modern-react-with-redux/"
 
-  },
-]
+    id: 3,
+    title: "The Complete React Developer Course (w/ Hooks and Redux)",
+    author: "Andrew Mead",
+    hours_video: 39,
+    number_of_lectures: 200,
+    rating: 4.7,
+    url: "https://codingthesmartway.com/courses/complete-react-web-app-developer/"
+
+  }
+];
 
 function App() {
   function getTestMsg(){
