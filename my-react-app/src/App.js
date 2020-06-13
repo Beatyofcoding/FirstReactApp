03e9b50b13cfb5907293ded130f6e5438fb7eaa9
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-06-13 23:04:23 +0200

    AddingSpaceToSeperateCorrectelyBetweenEachProperty

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -44,9 +44,9 @@ function App() {
                   <a href={course.url}><h4>{course.title}></h4></a>
                 </span>
               <span>By <strong>{course.author}</strong></span>
-              <span>Video Hours: {course.hours_video}</span>
-              <span>Number Of Lectures: {course.number_of_lectures}</span>
-              <span>Rating: {course.rating}</span>
+              <span> | Video Hours: {course.hours_video}</span>
+              <span> | Number Of Lectures: {course.number_of_lectures}</span>
+              <span> | Rating: {course.rating}</span>
                 </div>
               )
             })
