512a3175f7ecbe1e8e52fef5b84ff299882c2ec8
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-06-15 16:57:53 +0200

    MovingFunct(CoursesList)FromApp.jsToCoursesList.js

--- my-react-app/src/CoursesList.js
+++ my-react-app/src/CoursesList.js
@@ -1,2 +1,19 @@
 import React from "react";
 
+function CoursesList() {
+    return courses.map(function(course){
+      return (
+        <div key={course.id}>
+        <span>
+          <a href={course.url}><h4>{course.title}></h4></a>
+        </span>
+      <span>By <strong>{course.author}</strong></span>
+      <span> | Video Hours: {course.hours_video}</span>
+      <span> | Number Of Lectures: {course.number_of_lectures}</span>
+      <span> | Rating: {course.rating}</span>
+      <br/><br/>
+        </div>
+      );
+    });
+  
+  }
