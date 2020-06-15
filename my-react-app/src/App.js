5fcdbf215c5c9f0245906e9d14c378b2359924b9
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-06-15 16:41:33 +0200

    MakingFunc(CoursesList)

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -37,23 +37,27 @@ function App() {
     <div >
             <h1>Title Of Courses</h1>
             <hr />
-            {courses.map(function(course){
-              return (
-                <div key={course.id}>
-                <span>
-                  <a href={course.url}><h4>{course.title}></h4></a>
-                </span>
-              <span>By <strong>{course.author}</strong></span>
-              <span> | Video Hours: {course.hours_video}</span>
-              <span> | Number Of Lectures: {course.number_of_lectures}</span>
-              <span> | Rating: {course.rating}</span>
-              <br/><br/>
-                </div>
-              )
-            })
+            
             }
       </div>
   );
+}
+function CoursesList() {
+  return courses.map(function(course){
+    return (
+      <div key={course.id}>
+      <span>
+        <a href={course.url}><h4>{course.title}></h4></a>
+      </span>
+    <span>By <strong>{course.author}</strong></span>
+    <span> | Video Hours: {course.hours_video}</span>
+    <span> | Number Of Lectures: {course.number_of_lectures}</span>
+    <span> | Rating: {course.rating}</span>
+    <br/><br/>
+      </div>
+    );
+  });
+
 }
 
 export default App;
