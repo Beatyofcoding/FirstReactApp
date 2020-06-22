0918b732e08410c85d6b87fda8801fba2be3a388
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-06-22 15:42:36 +0200

    MovingLabel&InputLinesFromAppComponentToSearch.js

--- my-react-app/src/Search.js
+++ my-react-app/src/Search.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 const Search = ({onSearch}) => (
-    
+    <label htmlFor="searchInput">Search: </label>
+            <input id="searchInput" 
+                type="text" 
+                onChange={handleSearchInputChange}  />
 )
